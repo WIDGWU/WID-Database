635f1b1ff4f48d1e7a45826ddfa77e3c1fa2e6b7
Amit Shendge <92355828+Amitshendge@users.noreply.github.com>
2024-06-06 20:28:20 -0400

    Update urls.py

--- WID_UI/WID_UI/urls.py
+++ WID_UI/WID_UI/urls.py
@@ -43,7 +43,7 @@ urlpatterns = [
     path('get_cross_listed/', views.get_cross_listed, name='get_cross_listed'),
 
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    #path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
