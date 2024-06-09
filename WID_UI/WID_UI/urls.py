9c1c4557d52fa8abcaac7bfbbbdf6d2fd2206186
Amit Shendge <92355828+Amitshendge@users.noreply.github.com>
2024-06-09 13:49:34 -0400

    Update urls.py

--- WID_UI/WID_UI/urls.py
+++ WID_UI/WID_UI/urls.py
@@ -22,7 +22,7 @@ from rest_framework import permissions
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="API Documentation",
+      title="WID API Documentation",
       default_version='v1',
       description="Test description",
       terms_of_service="https://www.google.com/policies/terms/",
